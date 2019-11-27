49ce26340f2c7675d8a688301c80b97e13ee9130
Mike Hockerman <mike@hockerman.com>
2019-11-27 17:37:56 -0500

    Add html title.

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ const IndexPage = () => {
   return (
     <Fragment>
       <Head>
+        <title>Mike Hockerman</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono&display=swap" rel="stylesheet"></link>
