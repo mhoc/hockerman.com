145f4ae63102470b9b4ac1d59600f61e92e973fe
Mike Hockerman <mike@hockerman.com>
2019-12-28 12:31:32 -0500

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -rf ./out
-npm run build
-next export
-AWS_PROFILE=personal aws s3 sync ./out s3://mhoc-co-website 
-rm -rf ./out
-AWS_PROFILE=personal aws cloudfront create-invalidation \
-  --distribution-id EO1O33M2R2N09 \
-  --paths "/*"
