c3b0c1193579e30555e6068c383d9b5b98f6a9b7
Mike Hockerman <mike@hockerman.com>
2020-05-07 10:28:39 -0400

    Update JSONBio.tsx

--- components/bio/JSONBio.tsx
+++ components/bio/JSONBio.tsx
@@ -5,7 +5,6 @@ import BodyJSON from "../BodyJSON";
 const data = [
   { kind: "comment", comment: "contact" },
   { kind: "item", key: "email", value: "mike@hockerman.com", href: "mailto:mike@hockerman.com" },
-  { kind: "item", key: "keybase", value: "mhoc", href: "https://keybase.io/mhoc", isExternal: true },
   { kind: "br" },
   { kind: "comment", comment: "profiles" },
   { kind: "item", key: "github", value: "mhoc", href: "https://github.com/mhoc", isExternal: true },
