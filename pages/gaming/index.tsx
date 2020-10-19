ad9ae0304c865d5b73783eeb408a42f4917de3a9
Mike Hockerman <mike@hockerman.com>
2020-10-19 12:47:10 -0400

    Reorg some files.

--- pages/gaming/index.tsx
+++ pages/gaming/index.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
-import BasePage from "../components/BasePage";
-import GamingProfiles from "../components/gaming/GamingProfiles";
+import BasePage from "../../components/BasePage";
+import GamingProfiles from "../../components/gaming/GamingProfiles";
 
 const GamingPage = () => {
   const [ selectedTab, onSelectedTabChange ] = useState("profiles");
   return (
     <BasePage
-      header="cat profiles.md"
+      header="./profiles"
       nav={[{ label: "home", href: "/" }, { label: "gaming" } ]}
       onClickTab={onSelectedTabChange}
       selectedTab={selectedTab}
