4814a3a45ca8944cb453c1bf30726d79f8ed4a2a
Mike Hockerman <mike@hockerman.com>
2020-10-20 14:07:46 -0400

    Add best of 2010s decade list.

--- pages/gaming/index.tsx
+++ pages/gaming/index.tsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 
 import BasePage from "../../components/BasePage";
+import BodyJSON from "../../components/BodyJSON";
 import GamingProfiles from "../../components/gaming/GamingProfiles";
 
 const GamingPage = () => {
   const [ selectedTab, onSelectedTabChange ] = useState("profiles");
   return (
     <BasePage
-      header="./profiles"
+      header="./gaming"
       nav={[{ label: "home", href: "/" }, { label: "gaming" } ]}
       onClickTab={onSelectedTabChange}
       selectedTab={selectedTab}
-      tabs={["profiles"]}
+      tabs={["profiles", "opinion"]}
     >
       {selectedTab === "profiles" && <GamingProfiles />}
+      {selectedTab === "opinion" && (
+        <BodyJSON data={[
+          { kind: "item", key: "best_of_2010s", value: "./bestof2010s", href: "/gaming/bestof2010s" }, 
+        ]} />
+      )}
     </BasePage>
   );
 }
