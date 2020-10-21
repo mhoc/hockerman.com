e77b5ef1675c3f75b8cf98fea352534b149dea47
Mike Hockerman <mike@hockerman.com>
2020-10-21 15:05:16 -0400

    Move bio component into root. Just simplifying things.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
 import BasePage from "../components/BasePage";
-import JSONBio from "../components/bio/JSONBio";
+import BodyJSON from "../components/BodyJSON";
+
+const data = [
+  { kind: "comment", comment: "contact" },
+  { kind: "item", key: "email", value: "mike@hockerman.com", href: "mailto:mike@hockerman.com" },
+  { kind: "item", key: "github", value: "mhoc", href: "https://github.com/mhoc", isExternal: true },
+  { kind: "item", key: "twitter", value: "@mikehockerman", href: "https://twitter.com/MikeHockerman", isExternal: true },
+  { kind: "item", key: "resume.pdf", value: "download", href: "/resume-mike-hockerman.pdf" },
+  { kind: "br" },
+  { kind: "comment", comment: "content" },
+  { kind: "item", key: "gaming", value: "/gaming", href: "/gaming" },
+  { kind: "item", key: "one", value: "/one", href: "/one" },
+];
 
 const IndexPage = () => {
   return (
     <BasePage header="cat ./mike_hockerman.json" nav={[{label:"home"}]}>
-      <JSONBio />
+      <BodyJSON data={data} />
     </BasePage>
   );
 }
