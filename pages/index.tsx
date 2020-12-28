9280e55905904a7d4cd94d33c32a9d985ccc7e85
Mike Hockerman <mike@hockerman.com>
2020-12-28 15:54:02 -0500

    Slight reorg

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,20 +4,19 @@ import BasePage from "../components/BasePage";
 import BodyJSON from "../components/BodyJSON";
 
 const data = [
-  { kind: "comment", comment: "contact" },
   { kind: "item", key: "email", value: "mike@hockerman.com", href: "mailto:mike@hockerman.com" },
   { kind: "item", key: "github", value: "mhoc", href: "https://github.com/mhoc", isExternal: true },
   { kind: "item", key: "twitter", value: "@mikehockerman", href: "https://twitter.com/MikeHockerman", isExternal: true },
+  { kind: "array.single-line", key: "currency", items: [
+    { value: "btc", href: "/crypto/btc" },
+    { value: "eth", href: "/crypto/eth" },
+    { value: "xlm", href: "/crypto/xlm" },
+  ]},
   { kind: "item", key: "resume.pdf", value: "download", href: "/resume-mike-hockerman.pdf" },
   { kind: "br" },
   { kind: "comment", comment: "content" },
   { kind: "item", key: "gaming", value: "/gaming", href: "/gaming" },
   { kind: "item", key: "one", value: "/one", href: "/one" },
-  { kind: "br" },
-  { kind: "comment", comment: "crypto" },
-  { kind: "item", key: "btc", value: "3LEkvkmd...", href: "/crypto/btc" },
-  { kind: "item", key: "eth", value: "0xC6a3d6...", href: "/crypto/eth" },
-  { kind: "item", key: "xlm", value: "GDQP2KPQ...", href: "/crypto/xlm" },
 ];
 
 const IndexPage = () => {
