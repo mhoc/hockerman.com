6103dc73cfa76f0e914772af1f90bb92e575b72e
Mike Hockerman <mike@hockerman.com>
2021-03-02 12:21:02 -0500

    Remove some extra internal links.

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,6 @@ const data = [
   { kind: "item", key: "twitter", value: "@mikehockerman", href: "https://twitter.com/mikehockerman", isExternal: true },
   { kind: "item", key: "github", value: "mhoc", href: "https://github.com/mhoc", isExternal: true },
   { kind: "item", key: "resume.pdf", value: "download", href: "/resume-mike-hockerman.pdf" },
-  { kind: "br" },
-  { kind: "comment", comment: "content" },
-  { kind: "item", key: "gaming", value: "/gaming", href: "/gaming" },
-  { kind: "item", key: "one", value: "/one", href: "/one" },
 ];
 
 const IndexPage = () => {
