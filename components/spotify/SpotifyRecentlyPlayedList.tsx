49ef2a0b5b60b27a2d8a45316cdb6809f498fabc
Mike Hockerman <mike@hockerman.com>
2021-11-23 17:25:23 -0500

    Loading state for play history.

--- components/spotify/SpotifyRecentlyPlayedList.tsx
+++ components/spotify/SpotifyRecentlyPlayedList.tsx
@@ -1,13 +1,25 @@
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import times from "lodash/times";
 
 import { useSpotifyRecentlyPlayed } from "../hooks/useSpotifyRecentlyPlayed";
-import { TextDeemph, TextLink } from "../text";
+import { TextDeemph, TextLink, TextLoading } from "../text";
 
 export const SpotifyRecentlyPlayedList = () => {
   const srp = useSpotifyRecentlyPlayed();
   return (
     <>
       <div className="container">
+        {srp.state === "loading" && (
+          times(4, () => (
+            <div>
+              <TextDeemph>[&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;]</TextDeemph>
+              &nbsp;
+              <TextLoading />
+              &nbsp;
+              <TextLink hideUnderline>{">"}</TextLink>
+            </div>
+          ))
+        )}
         {srp.state === "results" && (
           srp.recentlyPlayed.slice(0, 4).map(rp => (
             <div>
@@ -24,8 +36,11 @@ export const SpotifyRecentlyPlayedList = () => {
       </div>
       <style jsx>{`
         .container {
+          align-items: space-between;
+          justify-content: space-between;
           display: flex;
           flex-direction: column;
+          height: 85px;
         }
       `}</style>
     </>
