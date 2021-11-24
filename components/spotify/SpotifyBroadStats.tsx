3ecd4006280f158f221b683470533fc6044986fa
Mike Hockerman <mike@hockerman.com>
2021-11-24 14:06:43 -0500

    Loading state.

--- components/spotify/SpotifyBroadStats.tsx
+++ components/spotify/SpotifyBroadStats.tsx
@@ -1,34 +1,38 @@
 import { useSpotifyBroadStats } from "../hooks/useSpotifyBroadStats";
-import { TextDeemph, TextStd } from "../text";
+import { TextDeemph, TextLoading, TextStd } from "../text";
 
 export const SpotifyBroadStats = () => {
   const sbs = useSpotifyBroadStats();
   return (
     <>
       <div className="container">
-        {sbs.state === "results" && (
-          <div>
-            <span>
-              <TextDeemph>Plays (Past 24 Hours):</TextDeemph>&nbsp;
-              <TextStd glow>{sbs.playsInLast24Hours}</TextStd>
-            </span>
-            <br />
-            <span>
-              <TextDeemph>Top Artists (Past Week):</TextDeemph>&nbsp;
-              <TextStd glow>
-                {sbs.topArtistsInLastWeek[0]}
-              </TextStd>
-              <TextDeemph>, </TextDeemph>
-              <TextStd glow>
-                {sbs.topArtistsInLastWeek[1]}
-              </TextStd>
-              <TextDeemph>, and </TextDeemph>
-              <TextStd glow>
-                {sbs.topArtistsInLastWeek[2]}
-              </TextStd>
-            </span>
-          </div>
-        )}
+        <div>
+          <span>
+            <TextDeemph>Plays (Past 24 Hours):</TextDeemph>&nbsp;
+            {sbs.state === "loading" && <TextLoading />}
+            {sbs.state === "results" && <TextStd glow>{sbs.playsInLast24Hours}</TextStd>}
+          </span>
+          <br />
+          <span>
+            <TextDeemph>Top Artists (Past Week):</TextDeemph>&nbsp;
+            {sbs.state === "loading" && <TextLoading />}
+            {sbs.state === "results" && (
+              <span>
+                <TextStd glow>
+                  {sbs.topArtistsInLastWeek[0]}
+                </TextStd>
+                <TextDeemph>, </TextDeemph>
+                <TextStd glow>
+                  {sbs.topArtistsInLastWeek[1]}
+                </TextStd>
+                <TextDeemph>, and </TextDeemph>
+                <TextStd glow>
+                  {sbs.topArtistsInLastWeek[2]}
+                </TextStd>
+              </span>
+            )}
+          </span>
+        </div>
       </div>
       <style jsx>{`
         .container {
