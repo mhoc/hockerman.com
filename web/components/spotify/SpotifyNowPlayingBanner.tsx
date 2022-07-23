0f05e2fc4236867e86e98e08343f875a0e08bd0a
Mike Hockerman <mike@hockerman.com>
2022-07-23 18:55:04 -0400

    Minor improvement to mobile rendering

--- web/components/spotify/SpotifyNowPlayingBanner.tsx
+++ web/components/spotify/SpotifyNowPlayingBanner.tsx
@@ -4,6 +4,7 @@ import { truncate } from "lodash";
 
 import { SpotifyNowPlayingHookResult } from "../hooks/useSpotifyNowPlaying";
 import { TextDeemph, TextLink, TextLoading, TextSubheader } from "../text";
+import { COOKIE_NAME_PRERENDER_BYPASS } from "next/dist/server/api-utils";
 
 interface Props {
   snp: SpotifyNowPlayingHookResult;
@@ -13,35 +14,61 @@ export const SpotifyNowPlayingBanner = ({ snp }: Props) => {
   return (
     <>
       <div className="container">
-        <TextSubheader>
-          <div className="spotify-icon">
-            <FontAwesomeIcon icon={faSpotify} />
-          </div>
-        </TextSubheader>
-        &nbsp;
-        {snp.state === "loading" && <TextLoading />}
-        {snp.state === "nothing" && <TextDeemph>Now Playing: Nothing :)</TextDeemph>}
-        {(snp.state === "playing" || snp.state === "paused") && (
-          <>
+        <div className="song-title">
+          <TextSubheader>
+            <div className="spotify-icon">
+              <FontAwesomeIcon icon={faSpotify} />
+            </div>
+          </TextSubheader>
+          &nbsp;
+          {snp.state === "loading" && <TextLoading />}
+          {snp.state === "nothing" && <TextDeemph>Now Playing: Nothing :)</TextDeemph>}
+          {(snp.state === "playing" || snp.state === "paused") && (
             <TextLink href={snp.trackHref} target="_blank" rel="noopener">{truncate(snp.track, { length: 40})}</TextLink>
-            &nbsp;<TextDeemph>by</TextDeemph>&nbsp;
-            <TextLink href={snp.artistHref} target="_blank" rel="noopener">{truncate(snp.artist, { length: 20 })}</TextLink>
-            {snp.album !== snp.track && (
-              <>
-                &nbsp;<TextDeemph>on</TextDeemph>&nbsp;
-                <TextLink href={snp.albumHref} target="_blank" rel="noopener">{truncate(snp.album, { length: 30})}</TextLink>
-              </>
-            )}
-            &nbsp;
+          )}&nbsp;
+        </div>
+        <div className="artist">
+          {(snp.state === "playing" || snp.state === "paused") && (
+            <span>
+              <TextDeemph>by</TextDeemph>&nbsp;
+              <TextLink href={snp.artistHref} target="_blank" rel="noopener">{truncate(snp.artist, { length: 20 })}</TextLink>
+            </span>
+          )}&nbsp;
+        </div>
+        <div className="album">
+          {(snp.state === "playing" || snp.state === "paused") && (
+            <span>
+              <TextDeemph>on</TextDeemph>&nbsp;
+              <TextLink href={snp.albumHref} target="_blank" rel="noopener">{truncate(snp.album, { length: 30})}</TextLink>
+            </span>
+          )}&nbsp;
+        </div>
+        <div className="playcounter">
+          {(snp.state === "playing" || snp.state === "paused") && (
             <TextDeemph>({snp.progressCurrentFmt}/{snp.durationFmt})</TextDeemph>
-          </>
-        )}
+          )}
+        </div>
       </div>
       <style jsx>{`
         .container {
           align-items: center;
           display: flex;
           flex-direction: row;
+        }
+        @media only screen and (max-width: 600px) {
+          .container {
+            align-items: start;
+            display: flex;
+            flex-direction: column;
+          }
+          .playcounter {
+            display: none;
+          }
+        }
+        .song-title {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
         }
         .spotify-icon {
           align-items: center;
