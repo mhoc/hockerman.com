7f2d8ede16b5493ebb4a16e390e089d474cffaca
Mike Hockerman <mike@hockerman.com>
2024-01-01 12:37:16 -0500

    Ok that was fun

--- web/astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import react from "@astrojs/react";
-import { defineConfig } from "astro/config";
-import icon from "astro-icon";
-
-export default defineConfig({
-  integrations: [icon(), react()],
-});
