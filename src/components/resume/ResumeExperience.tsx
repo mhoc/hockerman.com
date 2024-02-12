18f80e2f5e28fb0bedf7f232a713a15ad2be0550
Mike Hockerman <mike@hockerman.com>
2024-02-12 01:40:22 -0500

    Add more experience items, bring in framer motion, filters.

---
+++ src/components/resume/ResumeExperience.tsx
@@ -0,0 +1,74 @@
+interface Props {
+  borderColor: string;
+  bullets: Array<string>;
+  company: string;
+  companyHref?: string;
+  location: string;
+  positions: Array<{
+    title: string;
+    years: string;
+  }>;
+}
+
+const ResumeExperience = ({
+  borderColor,
+  bullets,
+  company,
+  companyHref,
+  location,
+  positions,
+}: Props) => {
+  return (
+    <div
+      className="px-4 py-2 transition border-r-zinc-900 hover:border-r-2 hover:border-r-zinc-700"
+      style={{
+        borderColor: borderColor,
+        borderLeftWidth: "2px",
+        borderRightWidth: "2px",
+      }}
+    >
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="flex flex-col">
+          <div className="flex flex-row items-baseline">
+            {companyHref ? (
+              <a href={companyHref} target="_blank">
+                <span className="text-lg lg:text-xl text-zinc-100 mr-3 leading-5">
+                  {company}
+                </span>
+              </a>
+            ) : (
+              <span className="text-lg lg:text-xl text-zinc-100 mr-3 leading-5">
+                {company}
+              </span>
+            )}
+            <span className="text-sm lg:text-md text-zinc-500 leading-5">
+              {location}
+            </span>
+          </div>
+          {positions.map((position) => (
+            <div className="flex flex-row items-baseline">
+              <span className="text-sm lg:text-md text-zinc-400 mr-3 leading-5">
+                {position.title}
+              </span>
+              {position.years && (
+                <span className="text-xs lg:text-sm text-zinc-500 leading-5">
+                  ({position.years})
+                </span>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col max-w-xl">
+          {bullets.map((bullet) => (
+            <div className="flex flex-row">
+              <span className="text-sm text-zinc-400 mr-2">•</span>
+              <span className="text-sm text-zinc-400">{bullet}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResumeExperience;
---
+++ src/components/resume/ResumeExperienceList.tsx
@@ -0,0 +1,239 @@
+import clsx from "clsx";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+
+import ResumeExperience from "./ResumeExperience";
+
+const ALL_ITEMS = [
+  {
+    borderColor: "#34d399",
+    bullets: [
+      "Architected, built, maintained, and evolved an array of TypeScript and Golang-based systems powering cybersecurity scanning for our customers, leveraging technologies such as Temporal, React, and open source scanning tools to identify vulnerabilities.",
+      "Balanced the prioritization of customer needs, business requirements, product evolution, and technical debt to meaningfully move the product suite forward.",
+      "Mentored a team of entry and mid-level engineers, assisting with architecture, decision making, documentation, career development, and implementation.",
+    ],
+    company: "Trava",
+    companyHref: "https://travasecurity.com",
+    location: "Indianapolis, IN",
+    positions: [{ title: "Senior Software Engineer", years: "2023-now" }],
+    tags: ["professional"],
+  },
+  {
+    borderColor: "#7c3aed",
+    bullets: [
+      "Planned & implemented back-of-house technical systems, including online accounts, mailing lists, domain management, and the primary marketing website.",
+    ],
+    company: "Women Investors Network",
+    companyHref: "https://womeninvestorsnetwork.org",
+    location: "Indianapolis, IN",
+    positions: [{ title: "Volunteer Web Engineer", years: "2019-now" }],
+    tags: ["community"],
+  },
+  {
+    borderColor: "#1d4ed8",
+    bullets: [
+      "Every year, the Xtern program matches hundreds of applicants for central Indiana technology internships to companies. Since ~2018, this matching has been automated using software I help execute and maintain.",
+    ],
+    company: "TechPoint",
+    companyHref: "https://techpoint.org",
+    location: "Indianapolis, IN",
+    positions: [{ title: "Volunteer Engineer", years: "2018-now" }],
+    tags: ["community"],
+  },
+  {
+    borderColor: "#fb923c",
+    bullets: [
+      "Architected & implemented new API and domains-facing platform features for enterprise customers including opt-in domain security enhancements and API access over mTLS.",
+      "Modernized dozens of legacy workloads from traditional servers to Kubernetes.",
+      "Responded to high severity on-call incidents affecting millions of customers and internet users, implementing observability and alerting improvements across our services.",
+    ],
+    company: "Cloudflare",
+    companyHref: "https://cloudflare.com",
+    location: "Remote",
+    positions: [{ title: "Systems Engineer", years: "2021-2023" }],
+    tags: ["professional"],
+  },
+  {
+    borderColor: "#f1f5f9",
+    bullets: [
+      "Practical Creatives was an Etsy store focused on the design, production, and sale of bespoke & useful 3D printed things.",
+    ],
+    company: "Practical Creatives",
+    location: "Indianapolis, IN",
+    positions: [{ title: "Founder", years: "2021-2023" }],
+    tags: ["hustles"],
+  },
+  {
+    borderColor: "#c084fc",
+    bullets: [
+      "Shipped countless product features spanning Structural’s Typescript & Node.JS backend and React frontend, including user sign-in and authorization, bulk user importing, business metrics tracking, the in-app admin panel, and more.",
+      "Owned and maintained all cloud deployed assets on AWS, evolving our deployment strategy over the years from a single EC2 instance, to adopting Docker, Kubernetes, and eventually AWS ECS.",
+      "Acted as engineering point-of-contact on security reviews and compliance, including four years of successful SOC-2 Type II audits.",
+      "Led the implementation of a variety of system monitoring and instrumentation capabilities, including alerting, refining our on-call procedures, logging, and distributed tracing, generally with Datadog.",
+    ],
+    company: "Structural",
+    companyHref: "https://structural.com",
+    location: "Indianapolis, IN",
+    positions: [
+      { title: "Senior Software Engineer", years: "2020-2021" },
+      { title: "Software Engineer", years: "2016-2020" },
+    ],
+    tags: ["professional"],
+  },
+  {
+    borderColor: "#15803d",
+    bullets: [
+      "After graduating from Purdue, I joined on to help build the first class of TechPoint's Tech Fellowship program; a two year post-grad program for tech workers in Indianapolis focusing on continuing education, social connections, and community outreach.",
+      "During my service, I helped build the foundations for the Tech Fellowship's curriculum program, including planning technology-oriented workshops for other members and running lunch-and-learns and fireside chats with local business leaders.",
+    ],
+    company: "Indy Tech Fellowship",
+    companyHref: "https://techpoint.org",
+    location: "Indianapolis, IN",
+    positions: [
+      { title: "Board Chair, Curriculum", years: "2016-2018" },
+      { title: "Member", years: "2016-2018" },
+    ],
+    tags: ["community"],
+  },
+  {
+    borderColor: "#fbbf24",
+    bullets: [
+      "Served as Vice President and President of Purdue's ACM Special Interest Group for Application Development (SIGAPP).",
+    ],
+    company: "Purdue University",
+    companyHref: "https://purdue.edu",
+    location: "West Lafayette, IN",
+    positions: [
+      { title: "B.S. Computer Science", years: "2012-2015" },
+      { title: "Minor, Organizational Leadership", years: "" },
+    ],
+    tags: ["edu"],
+  },
+  {
+    borderColor: "#ef4444",
+    bullets: [
+      "Optimized log search times in a MySQL-sourced data pipeline for business analytics, and engineered a mapping/filtering system to flag and censor personally identifiable information within the pipeline.",
+    ],
+    company: "Yelp",
+    companyHref: "https://yelp.com",
+    location: "San Francisco, CA",
+    positions: [{ title: "Software Engineering Intern", years: "2015" }],
+    tags: ["professional"],
+  },
+  {
+    borderColor: "#22d3ee",
+    bullets: [
+      "Shipped an end-to-end system which scanned PDF documents, applied optical character recognition and barcode scanning, and indexed the results in Elasticsearch.",
+    ],
+    company: "Genesys",
+    companyHref: "https://genesys.com",
+    location: "Indianapolis, IN",
+    positions: [{ title: "Software Engineering Intern", years: "2014" }],
+    tags: ["professional"],
+  },
+  {
+    borderColor: "#1d4ed8",
+    bullets: [
+      "Alongside my internship with Genesys, I participated in the first class of TechPoint's Xtern program.",
+    ],
+    company: "Xtern",
+    companyHref: "https://techpoint.org",
+    location: "Indianapolis, IN",
+    positions: [{ title: "Member", years: "2014" }],
+    tags: ["community"],
+  },
+];
+
+const ResumeExperienceList = () => {
+  const [filter, setFilter] = useState("all");
+  const visibilityTagged = ALL_ITEMS.map((item) => ({
+    isVisibile: filter === "all" ? true : item.tags.includes(filter),
+    ...item,
+  }));
+  return (
+    <div>
+      <div className="flex flex-row items-center">
+        <span className="text-zinc-300 mr-2">filter</span>
+        <span
+          className={clsx({
+            "border-zinc-300 border border-1 px-2 rounded-xl cursor-pointer mr-2 transition-all":
+              true,
+            "text-zinc-300": filter !== "all",
+            "text-zinc-800": filter === "all",
+            "bg-zinc-300": filter === "all",
+          })}
+          onClick={() => setFilter("all")}
+        >
+          all
+        </span>
+        <span
+          className={clsx({
+            "border-zinc-300 border border-1 px-2 rounded-xl cursor-pointer mr-2 transition-all":
+              true,
+            "text-zinc-300": filter !== "professional",
+            "text-zinc-800": filter === "professional",
+            "bg-zinc-300": filter === "professional",
+          })}
+          onClick={() => setFilter("professional")}
+        >
+          professional
+        </span>
+        <span
+          className={clsx({
+            "border-zinc-300 border border-1 px-2 rounded-xl cursor-pointer mr-2 transition-all":
+              true,
+            "text-zinc-300": filter !== "hustles",
+            "text-zinc-800": filter === "hustles",
+            "bg-zinc-300": filter === "hustles",
+          })}
+          onClick={() => setFilter("hustles")}
+        >
+          hustles
+        </span>
+        <span
+          className={clsx({
+            "border-zinc-300 border border-1 px-2 rounded-xl cursor-pointer mr-2 transition-all":
+              true,
+            "text-zinc-300": filter !== "edu",
+            "text-zinc-800": filter === "edu",
+            "bg-zinc-300": filter === "edu",
+          })}
+          onClick={() => setFilter("edu")}
+        >
+          edu
+        </span>
+        <span
+          className={clsx({
+            "border-zinc-300 border border-1 px-2 rounded-xl cursor-pointer mr-2 transition-all":
+              true,
+            "text-zinc-300": filter !== "community",
+            "text-zinc-800": filter === "community",
+            "bg-zinc-300": filter === "community",
+          })}
+          onClick={() => setFilter("community")}
+        >
+          community
+        </span>
+      </div>
+      <div className="mb-8" />
+      <div>
+        {visibilityTagged.map((item) => (
+          <AnimatePresence>
+            {item.isVisibile && (
+              <motion.div
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: -50 }}
+                initial={{ opacity: 0, x: -50 }}
+                key={item.company}
+              >
+                <ResumeExperience {...item} />
+              </motion.div>
+            )}
+          </AnimatePresence>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ResumeExperienceList;
