5a8e4a7511ebcd978cb9a07e8b01a6bfdd67fbd1
Mike Hockerman <mike@hockerman.com>
2024-10-08 14:55:40 -0400

    asdf

--- src/pages/api/apple_health.js
+++ src/pages/api/apple_health.js
@@ -2,9 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY,
-  "https://xyzcompany.supabase.co",
-  "public-anon-key"
+  process.env.SUPABASE_KEY
 );
 
 export async function POST({ request }) {
