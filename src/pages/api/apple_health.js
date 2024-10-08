1010b6be01733429c0be17242469f8fe0afb3fd9
Mike Hockerman <mike@hockerman.com>
2024-10-08 14:55:25 -0400

    asdf

--- src/pages/api/apple_health.js
+++ src/pages/api/apple_health.js
@@ -1,3 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY,
+  "https://xyzcompany.supabase.co",
+  "public-anon-key"
+);
+
 export async function POST({ request }) {
   const importToken = process.env.APPLE_HEALTH_IMPORT_TOKEN;
   if (!importToken) {
@@ -10,8 +19,21 @@ export async function POST({ request }) {
       status: 401,
     });
   }
+
   const body = await request.json();
-  console.log(JSON.stringify(body));
+  for (const metric of body?.data?.metrics ?? []) {
+    console.log(`importing metric=${metric.name} (${metric.unit})`);
+    for (const metricData of metric.data) {
+      const value = metricData.qty;
+      const at = new Date(metricData.date);
+      console.log(`inserting ${metric.name}+${at.toISOString()}`);
+      await supabase.from("apple_health_metrics").upsert({
+        at,
+        metric: metric.name,
+        value,
+      });
+    }
+  }
 
   return new Response("ok!", {
     status: 200,
