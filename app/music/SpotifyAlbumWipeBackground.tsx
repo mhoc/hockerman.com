2b6a8f7cd6892a95684a8431939782bcb5ddb2b1
Mike Hockerman <mike@hockerman.com>
2025-06-17 21:17:06 -0400

    Remove nextjs image components wtf

--- app/music/SpotifyAlbumWipeBackground.tsx
+++ app/music/SpotifyAlbumWipeBackground.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import Image from "next/image";
 import { getSpotifyAccessToken } from "../server/getSpotifyAccessToken";
 import { getSpotifyRecentlyPlayed } from "../server/getSpotifyRecentlyPlayed";
 import styles from "./SpotifyAlbumWipeBackground.module.css";
@@ -26,25 +26,17 @@ export async function SpotifyAlbumWipeBackground() {
       <div className="absolute top-0 left-0 h-screen w-screen -z-10 overflow-hidden">
         <div className="flex flex-row flex-wrap w-[calc(100vw+20vw)]">
           {currentlyPlayingBody.is_playing && (
-            <Image
-              alt="now-playing"
-              className={styles.bgimage}
-              height={350}
-              src={currentlyPlayingBody.item.album.images[0].url}
-              width={350}
-            />
+            <img alt="now-playing" className={styles.bgimage} src={currentlyPlayingBody.item.album.images[0].url} />
           )}
           {recentlyPlayedItems.map((item: any, i: number) => (
-            <Image
+            <img
               alt={`${item.artist} - ${item.album}`}
               className={styles.bgimage}
-              height={350}
               key={`${i}${item.track.album.images[0].url}`}
               src={item.track.album.images[0].url}
               style={{
                 animationDelay: `${0.05 * (i + 1)}s`,
               }}
-              width={350}
             />
           ))}
         </div>
--- app/music/SpotifyRecentlyPlayedCard.tsx
+++ app/music/SpotifyRecentlyPlayedCard.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import { IoTime } from "react-icons/io5";
 import { getSpotifyRecentlyPlayed } from "../server/getSpotifyRecentlyPlayed";
 
 export async function SpotifyRecentlyPlayedCard() {
@@ -10,17 +11,23 @@ export async function SpotifyRecentlyPlayedCard() {
       <div className="flex flex-row gap-8 flex-wrap">
         <div className="flex flex-col gap-1">
           {items.slice(0, 4).map((item: any) => (
-            <div className="flex flex-col" key={`${item.track.id}-${item.played_at}`}>
-              <span className="text-md text-gray-300 leading-none">{item.track.name}</span>
-              <span className="text-md text-gray-400 leading-none">{item.track.artists[0].name}</span>
+            <div className="flex flex-row gap-2 items-center" key={`${item.track.id}-${item.played_at}`}>
+              <IoTime className="text-emerald-400" />
+              <div className="flex flex-col">
+                <span className="text-md text-gray-300 leading-none">{item.track.name}</span>
+                <span className="text-md text-gray-400 leading-none">{item.track.artists[0].name}</span>
+              </div>
             </div>
           ))}
         </div>
         <div className="flex flex-col gap-1">
           {items.slice(4, 8).map((item: any) => (
-            <div className="flex flex-col" key={`${item.track.id}-${item.played_at}`}>
-              <span className="text-md text-gray-300 leading-none">{item.track.name}</span>
-              <span className="text-md text-gray-400 leading-none">{item.track.artists[0].name}</span>
+            <div className="flex flex-row gap-2 items-center" key={`${item.track.id}-${item.played_at}`}>
+              <IoTime className="text-emerald-400" />
+              <div className="flex flex-col">
+                <span className="text-md text-gray-300 leading-none">{item.track.name}</span>
+                <span className="text-md text-gray-400 leading-none">{item.track.artists[0].name}</span>
+              </div>
             </div>
           ))}
         </div>
