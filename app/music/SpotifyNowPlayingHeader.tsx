d373020436e7073ddac8cf07bbc45b1b0271bf43
Mike Hockerman <mike@hockerman.com>
2025-08-25 18:22:22 -0400

    Fix

--- app/music/SpotifyNowPlayingHeader.tsx
+++ app/music/SpotifyNowPlayingHeader.tsx
@@ -7,10 +7,10 @@ export async function SpotifyNowPlayingHeader() {
     return <span className="text-lg italic text-cobalt-200 mb-2">Nothing Playing...</span>;
   }
   const response = await currentlyPlayingResponse.json();
-  const title = `${response?.item.name}`;
-  const artist = `${response?.item.artists[0].name}`;
-  const album = `${response?.item.album.name}`;
-  const link = `${response?.item.external_urls?.spotify}`;
+  const title = `${response?.item?.name}`;
+  const artist = `${response?.item?.artists[0].name}`;
+  const album = `${response?.item?.album.name}`;
+  const link = `${response?.item?.external_urls?.spotify}`;
   return (
     <div className="flex flex-col gap-0">
       <a className="text-lg text-emerald-200 leading-none flex flex-row gap-2 items-center" href={link} target="_blank">
