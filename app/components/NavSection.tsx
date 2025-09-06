ec2eed6833d50b17701a0755e1e996bb372ebae3
Mike Hockerman <mike@hockerman.com>
2025-09-06 16:16:32 -0400

    Slightly better clickability on home page.

--- app/components/NavSection.tsx
+++ app/components/NavSection.tsx
@@ -1,6 +1,6 @@
 export function NavSection({ children, title }: { children: React.ReactNode; title?: string }) {
   return (
-    <div className="flex flex-col gap-0">
+    <div className="flex flex-col gap-1 md:gap-0">
       {title && <span className="font-serif font-semibold text-md text-rose-200 leading-tight select-none">{title}</span>}
       {children}
     </div>
